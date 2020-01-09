684a39b7ab0d913d8a7d97faddf577b42a6efa88
friday <friday@fridaydeMacBook-Pro.local>
2020-01-09 16:38:26 +0800

    update

--- .babelrc.js
+++ .babelrc.js
@@ -1,42 +1,22 @@
-const path = require('path');
-
-const baseDir = process.cwd() + '/node_modules/';
-
 module.exports = {
-  env: {
-    client: {
-      presets: [baseDir + '@babel/preset-env', baseDir + '@babel/preset-react'],
-      plugins: [
-        baseDir + '@babel/plugin-transform-runtime',
-        baseDir + '@babel/plugin-transform-spread',
-        baseDir + '@babel/plugin-proposal-class-properties',
-        baseDir + '@babel/plugin-proposal-export-default-from',
-        baseDir + '@babel/plugin-proposal-export-namespace-from',
-        baseDir + '@babel/plugin-syntax-dynamic-import',
-        baseDir + 'react-hot-loader/babel'
-      ]
-    },
-    server: {
-      presets: [baseDir + '@babel/preset-env', baseDir + '@babel/preset-react'],
-      plugins: [
-        baseDir + '@babel/plugin-transform-runtime',
-        baseDir + '@babel/plugin-transform-spread',
-        baseDir + '@babel/plugin-proposal-class-properties',
-        baseDir + '@babel/plugin-proposal-export-default-from',
-        baseDir + '@babel/plugin-proposal-export-namespace-from',
-        baseDir + '@babel/plugin-syntax-dynamic-import',
-        baseDir + 'babel-plugin-dynamic-import-node',
-        baseDir + 'babel-plugin-add-module-exports',
-        [
-          baseDir + 'babel-plugin-css-modules-transform',
-          {
-            generateScopedName: '[local]_[sha1:hash:base64:5]',
-            preprocessCss: path.resolve(__dirname, 'build/babel/sass-process'),
-            prepend: [baseDir + 'autoprefixer'],
-            extensions: ['.scss', '.css']
-          }
-        ]
-      ]
-    }
-  }
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-spread',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-syntax-dynamic-import',
+    'babel-plugin-dynamic-import-node',
+    'babel-plugin-add-module-exports',
+    [
+      'babel-plugin-css-modules-transform',
+      {
+        generateScopedName: '[local]_[sha1:hash:base64:5]',
+        preprocessCss: 'build/babel/sass-process',
+        prepend: ['autoprefixer'],
+        extensions: ['.scss', '.css']
+      }
+    ]
+  ]
 };

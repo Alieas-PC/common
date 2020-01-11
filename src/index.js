cab8b7c26aa3ab927b338669a2d9808c0fd9e4e2
friday <friday@fridaydeMacBook-Pro.local>
2020-01-11 17:27:29 +0800

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 const AppServer = require('./server/app');
 
-const { createLogger, getLogger } = require('./server/log');
-
-module.exports = { AppServer, createLogger, getLogger };
+module.exports = { AppServer };

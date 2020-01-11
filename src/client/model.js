b3c2b6d66d266c8f78e624b6497dd372cfd1c6f2
friday <friday@fridaydeMacBook-Pro.local>
2020-01-11 18:39:34 +0800

    update

--- src/client/model.js
+++ src/client/model.js
@@ -8,15 +8,14 @@ export default context => {
   const modelAccess = {};
 
   Object.keys(modelActions).forEach(k => {
-    modelAccess[k] = (...params) => {
-      const setPayload = {
-        ...params,
-        onSuccess: context.onSuccess ? context.onSuccess : null,
+    modelAccess[k] = (modelName, values) => {
+      store.dispatch(
+        modelActions[k](modelName, values, {
+          onSuccess: context.onSuccess ? context.onSuccess : null,
 
-        onError: context.onError ? context.onError : null
-      };
-
-      store.dispatch(modelActions[k](setPayload));
+          onError: context.onError ? context.onError : null
+        })
+      );
     };
   });
 
